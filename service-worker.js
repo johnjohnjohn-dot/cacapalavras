f0b2d2791d4394d2e0619efa58af6bd632b3fde5
johnjohnjohn-dot <johnmjft@gmail.com>
2026-08-23 14:06:20 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,47 @@
+const CACHE_NAME = "cacapalavras-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./app.js",
+    "./style.css"
+];
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+                .then(cache => {
+
+                    return cache.addAll(
+                        FILES_TO_CACHE
+                    );
+
+                })
+
+        );
+
+    }
+);
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            caches.match(event.request)
+                .then(response => {
+
+                    return response ||
+                        fetch(event.request);
+
+                })
+
+        );
+
+    }
+);
